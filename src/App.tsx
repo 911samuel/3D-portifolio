7734f73bff19e2e2b6d0c6fe5605d8ed28d13dc0
911samuel <abayizeraeaz@gmail.com>
2024-06-06 14:55:18 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import { About, Contact, Home, Projects}  from "./pages/index"
+
+
+function App() {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;
