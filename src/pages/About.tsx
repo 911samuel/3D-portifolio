dc450cf2a039aefe26aa1846a884b0b101aa1f2a
911samuel <abayizeraeaz@gmail.com>
2024-06-18 14:50:52 +0200

    finalise the project

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -57,7 +57,7 @@ const About = () => {
 
         <div className="mt-12 flex">
           <VerticalTimeline>
-            {experiences.map((experience, index) => (
+            {experiences.map((experience) => (
               <VerticalTimelineElement
                 key={experience.company_name}
                 date={experience.date}
